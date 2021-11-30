2cd8af52795e314a2963bff71121f19118502c67
Paul Ogolla <paulotieno2@gmail.com>
2021-11-30 08:23:44 +0300

    setup user model

---
+++ server/models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid')
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password cannot contain "Password"')
+      }
+    }
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
