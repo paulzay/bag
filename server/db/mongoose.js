4ef2c9c05f2134fe9161956ee1383c5da6860e0c
Paul Ogolla <paulotieno2@gmail.com>
2021-12-01 18:32:24 +0300

    remove deprecated params

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,4 +2,7 @@ const mongoose = require('mongoose')
 
 const connectionURL = 'mongodb://127.0.0.1:27017/bag'
 
-mongoose.connect(connectionURL, { useUnifiedTopology: true, useCreateIndex: true, useNewUrlParser: true })
+mongoose.connect(connectionURL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+})
