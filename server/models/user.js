60c14860e3af9defa28e79b3b3d36d89cf57fbdd
Paul Ogolla <paulotieno2@gmail.com>
2021-12-01 18:40:55 +0300

    implement user model and add bcrypt and jwt

--- server/models/user.js
+++ server/models/user.js
@@ -31,9 +31,30 @@ const userSchema = mongoose.Schema({
         throw new Error('Password cannot contain "Password"')
       }
     }
-  }
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
 
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+  next()
+})
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, 'secretkeyss')
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+  return token;
+}
 const User = mongoose.model('User', userSchema)
 
 module.exports = User;
