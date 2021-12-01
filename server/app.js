4bf627987b0696d4aa4bbd8f938ad3b3a237a84f
Paul Ogolla <paulotieno2@gmail.com>
2021-12-01 18:44:00 +0300

    import db and routes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const userRouter = require('./router/userRouter')
+require('./db/mongoose')
 
 const app = express();
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(userRouter)
 
 module.exports = app;
